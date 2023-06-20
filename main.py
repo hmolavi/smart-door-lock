e90137ae4545be8bd889c1de15d070e6212147e9
Hossein Molavi <75816912+hmolavi@users.noreply.github.com>
2023-06-20 00:22:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
+import threading
 import cv2
-from cvzone.FaceDetectionModule import FaceDetector
-from cvzone.SerialModule import SerialObject
+from deepface import DeepFace
+from cvzone import SerialModule
 
-cap = cv2.VideoCapture(2)
-detector = FaceDetector()
-arduino = SerialObject('/COM3')
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+arduino = SerialModule.SerialObject('/COM3', 9600)
+counter = 0
+face_match = False
+
+reference_img = cv2.imread("reference.jpg")  # Your own picture
+
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+
+    except ValueError:
+        face_match = False
+        
 
 while True:
-    success, img = cap.read()
-    img, faceBoxes = detector.findFaces(img)
+    ret, frame = cap.read()
 
-    if faceBoxes:
-        arduino.sendData([1])
-    else:
-        arduino.sendData([0])
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
 
-    cv2.imshow("Video", img)
-    cv2.waitKey(1)
+        if face_match:
+            arduino.send("Match: True")
+            cv2.putText(frame, "MATCH!", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            arduino.send("Match: False")
+            cv2.putText(frame, "NO MATCH!", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+arduino.close()
